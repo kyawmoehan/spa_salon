bcf1705138b35d55d5cdf997397f711e2c8934df
kyawmoehan <kyawmoehan012@gmail.com>
2022-07-08 21:30:23 +0630

    backup error

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -2,19 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class BackupController extends Controller
 {
-    public function __construct() 
+    public function __construct()
     {
         $this->middleware('role:admin');
     }
-    
+
     public function index()
     {
         return view('report.backup_database');
     }
+
     public function backupDatabase()
     {
 
@@ -24,9 +23,10 @@ class BackupController extends Controller
         $mysqlPassword      = env('DB_PASSWORD');
         $DbName             = env('DB_DATABASE');
         $backup_name        = "databackup.sql";
+
         $tables             = array("counter_items", "customers", "general_costs", "items", "item_lists", "item_vouchers", "purchase_items", "roles", "salaries", "services", "staff", "types", "usage_items", "users", "vouchers", "voucher_staff"); //here your tables...
 
-        $connect = new \PDO("mysql:host=$mysqlHostName;dbname=$DbName;charset=utf8", "$mysqlUserName", "$mysqlPassword",array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $connect = new \PDO("mysql:host=$mysqlHostName;dbname=$DbName;charset=utf8", "$mysqlUserName", "$mysqlPassword", array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
         $get_all_table_query = "SHOW TABLES";
         $statement = $connect->prepare($get_all_table_query);
         $statement->execute();
@@ -34,31 +34,43 @@ class BackupController extends Controller
 
 
         $output = '';
-        foreach($tables as $table)
-        {
-         $show_table_query = "SHOW CREATE TABLE " . $table . "";
-         $statement = $connect->prepare($show_table_query);
-         $statement->execute();
-         $show_table_result = $statement->fetchAll();
+        foreach ($tables as $table) {
+            $show_table_query = "SHOW CREATE TABLE " . $table . "";
+            $statement = $connect->prepare($show_table_query);
+            $statement->execute();
+            $show_table_result = $statement->fetchAll();
 
-         foreach($show_table_result as $show_table_row)
-         {
-          $output .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
-         }
-         $select_query = "SELECT * FROM " . $table . "";
-         $statement = $connect->prepare($select_query);
-         $statement->execute();
-         $total_row = $statement->rowCount();
+            foreach ($show_table_result as $show_table_row) {
+                $output .= "\n\n" . $show_table_row["Create Table"] . ";\n\n";
+            }
+            $select_query = "SELECT * FROM " . $table . "";
+            $statement = $connect->prepare($select_query);
+            $statement->execute();
+            $total_row = $statement->rowCount();
 
-         for($count=0; $count<$total_row; $count++)
-         {
-          $single_result = $statement->fetch(\PDO::FETCH_ASSOC);
-          $table_column_array = array_keys($single_result);
-          $table_value_array = array_values($single_result);
-          $output .= "\nINSERT INTO $table (";
-          $output .= "" . implode(", ", $table_column_array) . ") VALUES (";
-          $output .= "'" . implode("','", $table_value_array) . "');\n";
-         }
+            for ($count = 0; $count < $total_row; $count++) {
+                $single_result = $statement->fetch(\PDO::FETCH_ASSOC);
+                $table_column_array = array_keys($single_result);
+                $table_value_array = array_values($single_result);
+
+                $output .= "\nINSERT INTO $table (";
+                $output .= "" . implode(", ", $table_column_array) . ") VALUES (";
+                $numItems = count($table_value_array) - 1;
+                foreach ($table_value_array as $key => $value) {
+                    if ($value == null && $key == $numItems) {
+                        $output .= 'NULL';
+                    } else if ($value == null) {
+                        $output .= 'NULL' . ",";
+                    } else if ($key == $numItems) {
+                        $output .= "'" . $value . "'";
+                    } else {
+                        $output .= "'" . $value . "',";
+                    }
+                }
+                $output .= ");\n";
+                // $output .= "'" . implode("','", $table_value_array) . "');\n";
+                // dd($output);
+            }
         }
         $file_name = 'database_backup_on_' . date('y-m-d') . '.sql';
         $file_handle = fopen($file_name, 'w+');
@@ -70,12 +82,11 @@ class BackupController extends Controller
         header('Content-Transfer-Encoding: binary');
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
-           header('Pragma: public');
-           header('Content-Length: ' . filesize($file_name));
-           ob_clean();
-           flush();
-           readfile($file_name);
-           unlink($file_name);
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file_name));
+        ob_clean();
+        flush();
+        readfile($file_name);
+        unlink($file_name);
     }
-
 }
